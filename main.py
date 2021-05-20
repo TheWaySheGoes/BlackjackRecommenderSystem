c29accbd06f6fedf36a6cf602e4dd1b61e1afe3c
Lukasz <lukasz.kurasinski@gmail.com>
2021-05-20 08:28:55 +0200

    moved genetic testing to the genetic file

--- main.py
+++ main.py
@@ -12,31 +12,7 @@ test#first test
 
 
 
-#testing the genetic module
-data_vals=[[1.0, 1.0, 1.0],
-    [2.0,2.0,2.0],
-    [3.0,3.0,3.0],
-    [4.0,4.0,4.0],
-    [5.0,5.0,5.0]]
-data_labels=[[0.0,0.0],
-            [0.0,0.0],
-            [2.0,2.0],
-            [1.0,1.0],
-            [1.0,1.0]]
-output_size=len(data_labels[0])
-print(output_size)
-ga = Genetic_algo(500,output_size)
 
-print(len(data_vals))
-print(data_vals[0])
-
-
-for i in range(0,len(data_vals)):
-    ga.set_data(data_vals[i],data_labels[i])
-    ga.learn()  
-ga.predict([1.0,2.0,3.0])
-
-###############
 
 
 
